90a026443e55f3c6f7fbfccce2a933ab450b8e5e
SanketKaadam <109967338+SanketKaadam@users.noreply.github.com>
2023-10-31 11:32:44 +0530

    Update HomeController.java

--- MyWebApp/src/main/java/com/example/demo/HomeController.java
+++ MyWebApp/src/main/java/com/example/demo/HomeController.java
@@ -10,10 +10,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController 
 {
 	@RequestMapping("home") // in URL we sent request for home, so our controller find home request bcoz of @requestMapping annotation
-	public ModelAndView home(Alien alien) // now client sending multiple data so insist of writing @Requestparam 3 times and accept data we use object to accept multiple data.
+	public ModelAndView home(Employee Employee) // now client sending multiple data so insist of writing @Requestparam 3 times and accept data we use object to accept multiple data.
 	{
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("obj",alien);
+		mv.addObject("obj",Employee);
 		mv.setViewName("home.jsp");
 		return mv; // this is automatically search page in webapp folder bcoz internally it mentioned that way, if you use different location for jsp pages then you need to configure that location
 	}
